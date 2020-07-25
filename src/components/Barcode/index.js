4ea5126c75c14ac3e45668b9fc2fe6e431ca7b95
Andrey <a.so777@live.ca>
2020-07-25 16:47:28 -0700

    Instead of drawing on canvas, draw an SVG

--- src/components/Barcode/index.js
+++ src/components/Barcode/index.js
@@ -23,18 +23,18 @@ const Barcode = ({ instance }) => {
           stampAnnot.X = 100;
           stampAnnot.Y = 250;
           stampAnnot.Width = 300;
-          stampAnnot.Height = 100;
-          // put your data URI here
-          stampAnnot.ImageData = svgRef.current.toDataURL();
+          stampAnnot.Height = 200;
+          var svg = new XMLSerializer().serializeToString(svgRef.current);
+          var encodedData = btoa(svg);
+          stampAnnot.ImageData = `data:image/svg+xml;base64,${encodedData}`;
           stampAnnot.Author = annotManager.getCurrentUser();
-
           annotManager.addAnnotation(stampAnnot);
           annotManager.redrawAnnotation(stampAnnot);
         }}
       >
         Stamp on a PDF
       </button>
-      <canvas width='300px' height='300px' ref={svgRef}></canvas>
+      <svg ref={svgRef}></svg>
     </div>
   );
 };
