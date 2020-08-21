c61eaf3a86bce33f36b58f5d598aa4e847708518
Andrey <a.so777@live.ca>
2020-08-21 15:04:20 -0700

    Added a dropdown for 2D barcode types

--- src/components/Barcode/index.js
+++ src/components/Barcode/index.js
@@ -1,16 +1,45 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import JsBarcode from 'jsbarcode';
 import './barcode.css';
 
 const Barcode = ({ instance }) => {
   const svgRef = useRef(null);
+  const formatRef = useRef(null);
+
+  const [errText, setErrText] = useState('');
 
   return (
     <div className="barcodeContainer">
       <h1>Barcode Generator</h1>
+      <h2>2D Barcode</h2>
+      <select id="barcodeType" title="CODE128" ref={formatRef}>
+        <option value="CODE128">CODE128 auto</option>
+        <option value="CODE128A">CODE128 A</option>
+        <option value="CODE128B">CODE128 B</option>
+        <option value="CODE128C">CODE128 C</option>
+        <option value="EAN13">EAN13</option>
+        <option value="EAN8">EAN8</option>
+        <option value="UPC">UPC</option>
+        <option value="CODE39">CODE39</option>
+        <option value="ITF14">ITF14</option>
+        <option value="ITF">ITF</option>
+        <option value="MSI">MSI</option>
+        <option value="MSI10">MSI10</option>
+        <option value="MSI11">MSI11</option>
+        <option value="MSI1010">MSI1010</option>
+        <option value="MSI1110">MSI1110</option>
+        <option value="pharmacode">Pharmacode</option>
+      </select>
       <input
         onChange={e => {
-          JsBarcode(svgRef.current, e.currentTarget.value);
+          try {
+            setErrText('');
+            JsBarcode(svgRef.current, e.currentTarget.value, {
+              format: formatRef.current.value,
+            });
+          } catch (err) {
+            setErrText(err);
+          }
         }}
         type="text"
       ></input>
@@ -32,7 +61,10 @@ const Barcode = ({ instance }) => {
       >
         Stamp on a PDF
       </button>
+      <text className='error'>{errText}</text>
       <canvas ref={svgRef}></canvas>
+      
+
     </div>
   );
 };
